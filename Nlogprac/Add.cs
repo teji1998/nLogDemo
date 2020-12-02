ab8c834dc3f14b1ae862175803153a98d2915ef8
“teji1998” <tejasvinirpk@gmail.com>
2020-12-03 01:52:05 +0530

    Added:Demo project on Nlog

---
+++ Nlogprac/Add.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nlogprac
+{
+    class Add
+    {
+
+        Logger logger = LogManager.GetCurrentClassLogger();
+        public int Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                logger.Error("Debug unsuccessuccessful : Sum()");
+            }
+            int c = a + b;
+            logger.Debug("Debug Success: " + c);
+            return c;
+        }
+
+
+    }
+}
---
+++ Nlogprac/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Nlogprac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Add add = new Add();
+                var result = add.Sum(0, 3);
+                Console.WriteLine(result);
+
+            }
+        }
+    }
+}
